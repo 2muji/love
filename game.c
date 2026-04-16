4c540adb05c3c0e337010615467226dd54f2305c
中村つむぎ <tsumu@nakamuratsumuginoMacBook-Air.local>
2026-04-16 14:42:40 +0900

    first commit

---
+++ game.c
@@ -0,0 +1,42 @@
+ //cc game.c libraylib.a -framework Cocoa -framework IOKit
+ #include "raylib.h"
+
+ int main(){
+    InitWindow(800,600, "love jamp!");
+    SetTargetFPS( 60 );
+
+    int y = 300;
+    int ys = 20;
+    int tx = 800;
+    while( ! WindowShouldClose() ){
+        BeginDrawing();
+        if(IsKeyPressed(KEY_SPACE)){
+            ys = 20;
+        }
+           ClearBackground(PINK);
+           DrawCircle(tx, 600-40, 40, BLACK);
+           DrawCircle(400, y ,40, SKYBLUE);
+           y = y - ys;
+           if(600-40 < y){
+            y = 600 - 40;
+            ys = 0 ;
+        }else{
+           ys = ys - 1;
+        };
+
+        tx = tx - 2;
+
+        int 💀 = CheckCollisionCircles(
+            (Vector2){400, y}, 40,
+            (Vector2){tx, 600-40}, 40
+        );
+
+        if(💀){
+            break;
+        }
+
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+ }
